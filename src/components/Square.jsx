47c43fc3bc48809afca65686317fb4a804f7d8e0
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-08-12 20:21:34 -0300

    fix: limit paragraph length

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -7,11 +7,17 @@ function Square({ name, id, images, info, about, page }) {
     <div
       key={id}
       className={"card p_about"}
+      onClick={(e) => {
+        // e.currentTarget.style.maxHeight = "10000vh";
+        // e.currentTarget.style.overflow = "auto";
+      }}
       style={{
         maxWidth: "49%",
         backgroundColor: "#2c2d36",
         fontFamily: "Segoe UI",
         lineHeight: "1.3",
+        // maxHeight: "150vh",
+        // overflow: "hidden",
       }}
     >
       <p className={"name"} style={{ fontFamily: "Arial" }}>
@@ -27,12 +33,15 @@ function Square({ name, id, images, info, about, page }) {
       />
 
       <div className="about">
-        {about.map((paragraph) => {
-          return (
-            <p className={""} key={nanoid()}>
-              {paragraph}
-            </p>
-          );
+        {about.map((paragraph, ind, all) => {
+          const part = all.slice(0, ind).join(" ");
+          if (part.length < 1600) {
+            return (
+              <p className={""} key={nanoid()}>
+                {paragraph}
+              </p>
+            );
+          }
         })}
       </div>
     </div>
