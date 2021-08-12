a4a4b7adb61287aeb6b5248a854d98dabd790039
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-08-12 16:41:10 -0300

    :sparkles: feat: design

---
+++ src/components/Board.jsx
@@ -0,0 +1,79 @@
+import Square from "./Square";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+function Board() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://naruto-api.herokuapp.com/")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return (
+      <>
+        <div
+          style={{
+            display: "flex",
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "#ffffff",
+            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            style={{ height: "70vh", maxWidth: "100vw" }}
+            src={"./Loading_icon.gif"}
+          />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div
+        className="card-container"
+        key={nanoid()}
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          // justifyContent: "center",
+          // alignItems: "center",
+          width: "100%",
+          maxWidth: "100%",
+        }}
+      >
+        {items.map(({ name, id, images, info, about, page }) => (
+          <Square
+            className="square"
+            name={name}
+            id={id}
+            images={images}
+            info={info}
+            about={about}
+            key={nanoid()}
+            page={page}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Board;
---
+++ src/components/Square.jsx
@@ -0,0 +1,43 @@
+import { nanoid } from "nanoid";
+
+function Square({ name, id, images, info, about, page }) {
+  // let isVisible = true;
+  // console.log({ page });
+  return (
+    <div
+      key={id}
+      className={"card p_about"}
+      style={{
+        maxWidth: "49%",
+        backgroundColor: "#2c2d36",
+        fontFamily: "Segoe UI",
+        lineHeight: "1.3",
+      }}
+    >
+      <p className={"name"} style={{ fontFamily: "Arial" }}>
+        <a href={page} target="_blank" style={{ color: "#4287f5" }}>
+          {name}
+        </a>
+      </p>
+
+      <div className="about">
+        {about.map((paragraph) => {
+          return (
+            <p className={""} key={nanoid()}>
+              {paragraph}
+            </p>
+          );
+        })}
+      </div>
+
+      <img
+        src={images[images.length - 1]}
+        alt={name}
+        width={500}
+        style={{ margin: "10px 0 0 0", textAlign: "center", width: "100%" }}
+      />
+    </div>
+  );
+}
+
+export default Square;
